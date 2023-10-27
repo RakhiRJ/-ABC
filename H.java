d35f7ec355a623d49bfd41d71d1230dc08a28698
RakhiRJ <144945767+RakhiRJ@users.noreply.github.com>
2023-10-27 21:53:40 +0530

    Add files via upload

---
+++ H.java
@@ -0,0 +1,18 @@
+package Stringprogram;
+
+public class H {
+	public static void main(String[] args) {
+		
+		String a[]= new String[3];
+		a[0]= "ABC";
+		a[1]="PQR";
+		a[2]="XYZ";
+		
+		for(int i=0 ;i<=2; i++) {
+			System.out.println(a[i]);
+			
+		}
+				
+	}
+
+}
